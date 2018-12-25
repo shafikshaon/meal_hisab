54d3dfff07ac8bba7f272a7448f242aacaf1119c
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-25 18:26:22 +0600

    built in message framework added

--- settings.py
+++ settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.contrib import messages
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -158,3 +160,12 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'meal_hisab/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'meal_hisab/media/')
+
+# built-in Message framework
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
