0303cbd002b780c53972c2a90a3df013cb42ef99
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-25 20:43:14 +0600

    login view added

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from accounts.forms import LoginForm
+
+
+class LoginView(View):
+    template_name = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.data['username'].lower(), password=form.data['password'])
+            print(user)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                messages.error(request, 'Username or password is wrong!')
+                return HttpResponseRedirect('/accounts/login/')
+
+        return render(request, self.template_name)
