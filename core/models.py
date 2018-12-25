72297dbf75001d05d12c050919cf61232ed791da
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-25 18:13:45 +0600

    status and timelog models added

--- core/models.py
+++ core/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from django.db import models
+
+
+class Status(models.Model):
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class TimeLog(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
