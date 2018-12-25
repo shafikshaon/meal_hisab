8a141205927ec878045a5cdf4b58035786deb0fb
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-25 20:42:06 +0600

    project restructured static files 404 related issue fixed

--- meal_hisab/settings.py
+++ meal_hisab/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.contrib import messages
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -97,7 +98,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'meal_hisab.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
