8da9d26f30d58b32b09654341f8f7a2373b555d1
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-25 20:42:43 +0600

    Custom User model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.models import TimeLog, Status
+
+
+class User(AbstractUser, TimeLog, Status):
+    email = models.EmailField(blank=False, null=False)
+    is_admin = models.BooleanField(default=False, null=False, blank=False)
+    member_from = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    class Meta:
+        app_label = "accounts"
+        db_table = "accounts_users"
+        verbose_name = "accounts_user"
+        verbose_name_plural = "accounts_users"
