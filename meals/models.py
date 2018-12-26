eba63914295c7bcb94b9e8901cdf99bebd57c0b2
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-26 20:50:47 +0600

    Meal model added

--- meals/models.py
+++ meals/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+from accounts.models import User
+from core.models import Status, TimeLog
+
+
+class Meal(TimeLog, Status):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='meals_user')
+    meal_date = models.DateField(blank=False, null=False)
+    breakfast = models.FloatField(default=0.5)
+    lunch = models.FloatField(default=1)
+    dinner = models.FloatField(default=1)
+
+    class Meta:
+        app_label = "meals"
+        db_table = "meals_meals"
+        verbose_name = "meals_meal"
+        verbose_name_plural = "meals_meals"
